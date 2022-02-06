1c6840533d087409567271ca7098c9054399d9f7
Michael <memarino92@gmail.com>
2022-02-06 13:08:55 -0500

    Added tailwind and prettier

--- WebStoreFrontend/src/app/app.module.ts
+++ WebStoreFrontend/src/app/app.module.ts
@@ -1,22 +1,20 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { environment } from 'src/environments/environment';
-import { API_BASE_URL } from 'src/shared/service-proxies/service-proxies';
+import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { environment } from 'src/environments/environment'
+import { API_BASE_URL } from 'src/shared/service-proxies/service-proxies'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [
+    {
+      provide: API_BASE_URL,
+      useFactory: () => environment.API_BASE_URL,
+    },
   ],
-  imports: [
-    BrowserModule, HttpClientModule
-  ],
-  providers: [{
-    provide: API_BASE_URL,
-    useFactory: () => environment.API_BASE_URL
-  }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
