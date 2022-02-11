89253e2210174a28134cec5875a9e1f0e6a68482
Michael <memarino92@gmail.com>
2022-02-10 22:26:33 -0500

    Book model changes

--- WebStoreAPI/Book.cs
+++ WebStoreAPI/Book.cs
@@ -5,12 +5,14 @@ namespace WebStoreAPI
         public int BookId { get; set; }
         public DateTime CreatedAt { get; set; }
 
-        public string Name { get; set; }
+        public string Title { get; set; }
 
         public string Author { get; set; }
         
         public  string? ImageUrl { get; set; }
 
         public string? Summary { get; set; }
+
+        public decimal? Price { get; set; }
     }
 }
