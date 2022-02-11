3f4f2509ac9013c23f9746cece614227ced295d9
Michael Marino <memarino92@gmail.com>
2022-02-11 11:58:47 -0500

    Updated tailwind config

--- WebStoreFrontend/tailwind.config.js
+++ WebStoreFrontend/tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
