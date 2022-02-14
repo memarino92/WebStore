3e699b25fbb5e04726d02bc8f7301eae17551c59
Michael <memarino92@gmail.com>
2022-02-13 21:45:31 -0500

    Ran prettier

--- WebStoreFrontend/src/app/app.module.ts
+++ WebStoreFrontend/src/app/app.module.ts
@@ -4,16 +4,24 @@ import { BrowserModule } from '@angular/platform-browser'
 import { environment } from 'src/environments/environment'
 import { API_BASE_URL } from 'src/shared/service-proxies/service-proxies'
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NavLinksComponent } from './nav-links/nav-links.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { FeaturedComponent } from './featured/featured.component';
-import { FooterComponent } from './footer/footer.component'
+import { AppComponent } from './app.component'
+import { NavbarComponent } from 'src/app/navbar/navbar.component'
+import { NavLinksComponent } from 'src/app/nav-links/nav-links.component'
+import { CarouselComponent } from 'src/app/carousel/carousel.component'
+import { CategoriesComponent } from 'src/app/categories/categories.component'
+import { FeaturedComponent } from 'src/app/featured/featured.component'
+import { FooterComponent } from 'src/app/footer/footer.component'
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, NavLinksComponent, CarouselComponent, CategoriesComponent, FeaturedComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    NavLinksComponent,
+    CarouselComponent,
+    CategoriesComponent,
+    FeaturedComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule, HttpClientModule],
   providers: [
     {
