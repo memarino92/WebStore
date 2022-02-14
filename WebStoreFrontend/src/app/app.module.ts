c704943b2e67e94742d3df1076662216d61f2939
Michael <memarino92@gmail.com>
2022-02-13 21:18:41 -0500

    Added categories component

--- WebStoreFrontend/src/app/app.module.ts
+++ WebStoreFrontend/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { API_BASE_URL } from 'src/shared/service-proxies/service-proxies'
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavLinksComponent } from './nav-links/nav-links.component';
-import { CarouselComponent } from './carousel/carousel.component'
+import { CarouselComponent } from './carousel/carousel.component';
+import { CategoriesComponent } from './categories/categories.component'
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, NavLinksComponent, CarouselComponent],
+  declarations: [AppComponent, NavbarComponent, NavLinksComponent, CarouselComponent, CategoriesComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [
     {
