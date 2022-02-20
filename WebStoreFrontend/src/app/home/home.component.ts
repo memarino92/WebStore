ab109e36af3497342cd7a57d9e30e1fe3575cae6
Michael <memarino92@gmail.com>
2022-02-20 18:48:49 -0500

    Home page using cloudinary transforms

--- WebStoreFrontend/src/app/home/home.component.ts
+++ WebStoreFrontend/src/app/home/home.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core'
+import { CloudinaryImage } from '@cloudinary/url-gen/assets/CloudinaryImage'
+import { Cloudinary } from '@cloudinary/url-gen'
+import { thumbnail } from '@cloudinary/url-gen/actions/resize'
+import { imageNameFromUrl } from 'src/shared/utils/utils'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  img!: CloudinaryImage
+
   constructor() {}
+  ngOnInit(): void {
+    const myCld = new Cloudinary({ cloud: { cloudName: 'mmarino' } })
+    this.img = myCld
+      .image(
+        imageNameFromUrl(
+          'https://res.cloudinary.com/mmarino/image/upload/v1644598562/counter_at_bookstore_mhfoxb.jpg'
+        )
+      )
+      .resize(thumbnail().width(731).height(384))
+  }
 }
