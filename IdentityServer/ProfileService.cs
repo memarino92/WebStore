07b84b34c10b47fafd02cc5c4cb573b758ddb9e8
Michael Marino <memarino92@gmail.com>
2022-02-28 16:30:56 -0500

    WIP on roles

---
+++ IdentityServer/ProfileService.cs
@@ -0,0 +1,39 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityModel;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace IdentityServer
+{
+    public class ProfileService : IProfileService
+    {
+        protected UserManager<ApplicationUser> _userManager;
+
+        public ProfileService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            ApplicationUser user = await _userManager.GetUserAsync(context.Subject);
+
+            IList<string> roles = await _userManager.GetRolesAsync(user);
+
+            IList<Claim> roleClaims = new List<Claim>();
+            foreach (string role in roles)
+            {
+                roleClaims.Add(new Claim(JwtClaimTypes.Role, role));
+            }
+
+            context.IssuedClaims.AddRange(roleClaims);
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- WebStoreAPI/Controllers/UserController.cs
+++ WebStoreAPI/Controllers/UserController.cs
@@ -11,13 +11,21 @@ namespace WebStoreAPI.Controllers
     {
         private readonly UserManager<User> _userManager;
 
+        private readonly RoleManager<IdentityRole> _roleManager;
+
         private readonly ILogger<UserController> _logger;
 
         private readonly WebStoreContext _webStoreContext;
 
-        public UserController(UserManager<User> userManager, ILogger<UserController> logger, WebStoreContext context)
+        public UserController(
+            UserManager<User> userManager, 
+            RoleManager<IdentityRole> roleManager,
+            ILogger<UserController> logger, 
+            WebStoreContext context
+            )
         {
             _userManager = userManager;
+            _roleManager = roleManager;
             _logger = logger;
             _webStoreContext = context;
         }
@@ -34,7 +42,14 @@ namespace WebStoreAPI.Controllers
         {
             var user = new User { Email = createUserDTO.Email, UserName = createUserDTO.UserName };
             var result = await _userManager.CreateAsync(user, createUserDTO.Password);
-            
+
+            bool adminRoleExists = await _roleManager.RoleExistsAsync("admin");
+            if (!adminRoleExists)
+            {
+                await _roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            var result2 = await _userManager.AddToRoleAsync(user, "admin");
+
             return user;
         }
     }
