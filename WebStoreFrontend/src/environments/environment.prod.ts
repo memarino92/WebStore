1a43cb1cb42f224f34fd9dfb719bed85c41bb129
Michael <memarino92@gmail.com>
2022-03-07 21:26:28 -0500

    build: convert angular auth to use .env

--- WebStoreFrontend/src/environments/environment.prod.ts
+++ WebStoreFrontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   API_BASE_URL: process.env.NG_APP_API_BASE_URL,
+  TOKEN_AUTHORITY: process.env.NG_APP_TOKEN_AUTHORITY,
 }
--- WebStoreFrontend/src/environments/environment.ts
+++ WebStoreFrontend/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   API_BASE_URL: process.env.NG_APP_API_BASE_URL,
+  TOKEN_AUTHORITY: process.env.NG_APP_TOKEN_AUTHORITY,
 }
 
 /*
