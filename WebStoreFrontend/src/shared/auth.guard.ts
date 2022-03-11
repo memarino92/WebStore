15e4b43347ef0dc40ab2da0a112c246ce0ab402c
Michael Marino <memarino92@gmail.com>
2022-03-11 09:35:25 -0500

    feat: add auth guard and add  offline_access to scope

---
+++ WebStoreFrontend/src/shared/auth.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core'
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router'
+
+import { OidcSecurityService } from 'angular-auth-oidc-client'
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authenticationService: OidcSecurityService
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const currentUser = this.authenticationService.getUserData()
+    if (currentUser) {
+      // check if route is restricted by role
+      if (
+        route.data.roles &&
+        route.data.roles.indexOf(currentUser.role) === -1
+      ) {
+        // role not authorised so redirect to home page
+        this.router.navigate(['/'])
+        return false
+      }
+
+      // authorised so return true
+      return true
+    }
+
+    // not logged in so redirect to login page with the return url
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } })
+    return false
+  }
+}
