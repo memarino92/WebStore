ed56c63df5762bd2051f12ce2a8eb49a3c705af3
Michael <memarino92@gmail.com>
2022-03-20 22:12:34 -0400

    feat: add edit book modal and button

--- WebStoreFrontend/src/app/admin-book-row/admin-book-row.component.ts
+++ WebStoreFrontend/src/app/admin-book-row/admin-book-row.component.ts
@@ -5,6 +5,8 @@ import { Cloudinary } from '@cloudinary/url-gen'
 import { thumbnail } from '@cloudinary/url-gen/actions/resize'
 import { imageNameFromUrl } from 'src/shared/utils/utils'
 import { ServiceProxy } from '../../shared/service-proxies/service-proxies'
+import { Subject } from 'rxjs'
+import * as mdb from 'mdb-ui-kit' // lib
 
 const FALLBACK_IMAGE_URL = `https://res.cloudinary.com/mmarino/image/upload/v1644550847/SimpleBook_h6isa6.svg`
 
@@ -18,6 +20,7 @@ const FALLBACK_IMAGE_URL = `https://res.cloudinary.com/mmarino/image/upload/v164
 export class AdminBookRowComponent implements OnInit {
   @Input() book!: CreateBookDTO
   @Input() index!: number
+  saveBookEventSubject: Subject<void> = new Subject<void>()
 
   img!: CloudinaryImage
 
@@ -36,5 +39,25 @@ export class AdminBookRowComponent implements OnInit {
 
   deleteBook() {
     this._serviceProxy.deleteBook(this.book.bookId).subscribe()
+  }
+
+  saveChanges() {
+    this.saveBookEventSubject.next()
+  }
+
+  updateBook(book: CreateBookDTO) {
+    this._serviceProxy.updateBook(book).subscribe(() => {
+      const modal = new mdb.Modal(
+        document.getElementById(`editBook${this.book.bookId}Modal`)
+      )
+      modal.hide()
+    })
+  }
+
+  openEditBookModal() {
+    const modal = new mdb.Modal(
+      document.getElementById(`editBook${this.book.bookId}Modal`)
+    )
+    modal.show()
   }
 }
--- WebStoreFrontend/src/shared/service-proxies/service-proxies.ts
+++ WebStoreFrontend/src/shared/service-proxies/service-proxies.ts
@@ -262,6 +262,62 @@ export class ServiceProxy {
     }
 
     /**
+     * @param body (optional) 
+     * @return Success
+     */
+    updateBook(body: CreateBookDTO | undefined): Observable<CreateBookDTO> {
+        let url_ = this.baseUrl + "/api/Catalog/UpdateBook";
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(body);
+
+        let options_ : any = {
+            body: content_,
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Content-Type": "application/json",
+                "Accept": "text/plain"
+            })
+        };
+
+        return this.http.request("put", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processUpdateBook(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processUpdateBook(response_ as any);
+                } catch (e) {
+                    return _observableThrow(e) as any as Observable<CreateBookDTO>;
+                }
+            } else
+                return _observableThrow(response_) as any as Observable<CreateBookDTO>;
+        }));
+    }
+
+    protected processUpdateBook(response: HttpResponseBase): Observable<CreateBookDTO> {
+        const status = response.status;
+        const responseBlob =
+            response instanceof HttpResponse ? response.body :
+            (response as any).error instanceof Blob ? (response as any).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = CreateBookDTO.fromJS(resultData200);
+            return _observableOf(result200);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<CreateBookDTO>(null as any);
+    }
+
+    /**
      * @return Success
      */
     getBooksForAdmin(): Observable<CreateBookDTO[]> {
